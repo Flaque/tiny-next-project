bab3068086463742b430e523e89bd78b9e2a6532
Flaque <evanjamesconrad@gmail.com>
2019-10-23 17:11:50 -0700

    Init commit

---
+++ pages/about.js
@@ -0,0 +1,5 @@
+export default () => (
+  <div>
+    <h1> I am an ABOUT WOOO page</h1>
+  </div>
+);
---
+++ pages/index.js
@@ -0,0 +1,6 @@
+export default () => (
+  <div>
+    <h1>hi Ethan!</h1>
+    <p>It's like html!</p>
+  </div>
+);
